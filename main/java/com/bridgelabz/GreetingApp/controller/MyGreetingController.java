c6fc9275ec7f3f21dc3926825df992d5c3cf7fa1
dsdhumale <d.sdhumale@gmail.com>
2023-01-15 23:54:26 +0530

    [Devendra]Added : Welcome to my greeting app

---
+++ main/java/com/bridgelabz/GreetingApp/controller/MyGreetingController.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.GreetingApp.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bridgelabz.GreetingApp.model.Greeting;
+
+@RestController
+public class MyGreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+    
+    @GetMapping(value = {"/greeting", "/greeting/home"})
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/param/{name}")
+    public Greeting greetingParam(@PathVariable String name){
+        return new Greeting(counter.incrementAndGet(),
+                   String.format(template, name));
+    }
+    
+}
